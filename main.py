a2cbdce0651e1de9d42e975ff87327d3a2fed562
Ankit Rawat <104831310+IamAnkitR@users.noreply.github.com>
2023-02-02 14:59:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Developed by Guido van Rossum in 1989-1992.
+# Python is a dynamically typed, general purpose and high-level programming language, which supports object-oriented as well as functional programming approach.
+# It is interpreted language (needs an interpreter) and platform-independent which makes debugging very easy.
+
+print("Hello World")
