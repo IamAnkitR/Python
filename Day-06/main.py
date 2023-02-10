f16a4ae6b5b23ebd9e8171b7ac5bf67cdf12e8ce
Ankit Rawat <104831310+IamAnkitR@users.noreply.github.com>
2023-02-10 17:33:09 +0000

    Refactor

--- Day-06/main.py
+++
@@ -1,6 +0,0 @@
-# Python has different types of operators for different operations. Operators are used to perform operations on variables and values.
-
-# visit -- https://www.w3schools.com/python/python_operators.asp
-
-
-
